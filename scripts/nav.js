43f421d10992d19fad1b85713c9caee445cd7e48
Kristoffer Linder <linderkristoffer@hotmail.com>
2025-02-07 10:26:11 +0100

    Start of project

---
+++ scripts/nav.js
@@ -0,0 +1,25 @@
+document.addEventListener("DOMContentLoaded", (event) => {
+    let navbar = `
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+    <div class="container-fluid">
+    <ul class="nav">
+  <li class="nav-item">
+    <a class="nav-link active" aria-current="page" href="index.html">Hem</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="about.html">Om Oss</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="menu.html">Meny</a>
+  </li>
+</ul></div></nav>`;
+
+    let footer = `<footer class="footer mt-auto py-3 bg-body-tertiary">
+        <div class="container">
+            <span class="text-body-secondary">Footer is placed here</span>
+        </div>
+    </footer>`;
+
+    document.getElementById("bodyId").insertAdjacentHTML("afterbegin", navbar);
+    document.getElementById("bodyId").insertAdjacentHTML("beforeend", footer);
+});
